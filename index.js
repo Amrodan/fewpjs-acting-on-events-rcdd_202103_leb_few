19381ac1f8336ce11fdc71cf636587f878828fda
Amro dandashli <amrdandashli@gmail.com>
2021-04-16 17:06:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px', '');
   console.log(leftNumbers)
   var left = parseInt(leftNumbers, 10)
-  if (left > 0) {
+  if (left >360) {
     dodger.style.left = `${left + 1}px`
   }
 }
